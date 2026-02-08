4d5e3b8c76c555391d373a8602eddc074d29a798
Andriy Prots <anriyredstone@gmail.com>
2026-02-08 13:07:05 +0200

    change 404 page

--- 404.php
+++ 404.php
@@ -18,6 +18,7 @@ get_header();
 
 	<div class="error-404 not-found default-max-width">
 		<div class="page-content">
+			<h1>404</h1>
 			<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try a search?', 'twentytwentyone' ); ?></p>
 			<?php get_search_form(); ?>
 		</div><!-- .page-content -->
